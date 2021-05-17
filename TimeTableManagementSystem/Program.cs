42dccf7da69206c688cdc937b54978bb122c5022
pawanikm <pawanimunasinghe99@gmail.com>
2021-05-18 00:27:20 +0530

    Add project files.

---
+++ TimeTableManagementSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new AddLecturers());
+            //Application.Run(new AddSessions());
+            //Application.Run(new AddSubject());
+            //Application.Run(new ManageSessions());
+            //Application.Run(new ManageLecturers());
+           //Application.Run(new ManageSubjects());
+
+        }
+    }
+}
